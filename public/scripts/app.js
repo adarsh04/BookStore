73ae2bd04cd52884d862f8f041415f7277e8e501
adarsh <adarsh.jegadeesan@acx.io>
2018-04-13 16:56:18 +1000

    Initialised app.

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var para = React.createElement(
+  'p',
+  null,
+  ' This is the book store '
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(para, appRoot);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+const para = (<p> This is the book store </p>);
+
+const appRoot = document.getElementById('app');
+ReactDOM.render(para, appRoot)
